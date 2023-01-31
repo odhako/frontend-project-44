9d03dc31ba7364fe877e085f5f34e22ee3d26132
odhako <odhako@gmail.com>
2023-01-31 09:50:55 +0400

    Refactoring

---
+++ src/games/even.js
@@ -0,0 +1,15 @@
+const gameEven = (numberOfGames, maxRandomNumber) => {
+  const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const games = [];
+  const gameContents = [rules, games];
+
+  for (let i = 0; i < numberOfGames; i += 1) {
+    const randomNumber = Math.floor(Math.random() * maxRandomNumber);
+    const rightAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
+    games.push([randomNumber, rightAnswer])
+  }
+
+  return gameContents;
+};
+
+export default gameEven;

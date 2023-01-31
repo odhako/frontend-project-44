ada285a25e05a57a4f305d62b40032501d208fc5
odhako <odhako@gmail.com>
2023-01-31 12:15:52 +0400

    Add brain-gcd game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import gameEngine from '../src/index.js';
+import gameGcd from '../games/gcd.js';
+
+gameEngine(gameGcd);
---
+++ games/gcd.js
@@ -0,0 +1,32 @@
+const calcGcd = (number1, number2) => {
+  let gcd = 1;
+  let i = 1;
+  while (i <= number1 || i <= number2) {
+    if (number1 % i === 0 && number2 % i === 0) {
+      gcd = i;
+    }
+    i += 1;
+  }
+
+  return gcd;
+};
+
+const gameGcd = (numberOfGames, maxRandomNumber) => {
+  const rules = 'Find the greatest common divisor of given numbers.';
+  const games = [];
+  const gameContents = [rules, games];
+
+  for (let i = 0; i < numberOfGames; i += 1) {
+    const randomNumber = () => Math.floor(Math.random() * maxRandomNumber);
+    const number1 = randomNumber();
+    const number2 = randomNumber();
+    const question = `${number1} ${number2}`;
+    const rightAnswer = calcGcd(number1, number2).toString();
+
+    games.push([question, rightAnswer]);
+  }
+
+  return gameContents;
+};
+
+export default gameGcd;
